e7bb9cee7c56e69185d7088565eb41d3f1d4943f
Rosie-Chenyr <146391133+Rosie-Chenyr@users.noreply.github.com>
2024-05-31 19:47:17 -0700

    Update product_item.py

--- pages/product_item.py
+++ pages/product_item.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 @st.cache()
 def load_data(path):
-    data = pd.read_json("/Users/chenyarou/Desktop/output_data_item.json", lines=True)
+    data = pd.read_json(path, lines=True)
     data['average_rating'] = pd.to_numeric(data['average_rating'], errors='coerce')
     data['rating_number'] = pd.to_numeric(data['rating_number'], errors='coerce')
     data['main_category'] = data['main_category'].astype(str)
@@ -14,14 +14,17 @@ def load_data(path):
     return data.copy()
 
 # read the data
-data = load_data('output_data_item.jsonl')
+data = load_data('https://raw.githubusercontent.com/arashi520/Amazon-Review-Analysis/main/data/output_data_item.jsonl')
+
+data_cleaned = data.dropna(subset=['store'])
 
 # 筛选 rating_number 最多的前十个 store
-top_stores = data['store'].value_counts().head(10)
+top_stores = data_cleaned['store'].value_counts().head(10)
 
 # 创建条形图显示前十个 store 的评分数量
 st.write("### Top 10 Stores by Rating Number")
 st.bar_chart(top_stores)
+
 
 # 筛选 rating_number 大于 50 的产品，并绘制 average_rating 的分布图
 filtered_data = data[data['rating_number'] > 50]
@@ -32,5 +35,7 @@ st.plotly_chart(fig)
 # 筛选 rating_number 最多的前 100 个产品的 title，并生成 word cloud
 top_titles = data.sort_values('rating_number', ascending=False).head(100)['title']
 wordcloud = WordCloud(width=800, height=400, background_color ='white').generate(' '.join(top_titles))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
 st.write("### Word Cloud of Top 100 Titles by Rating Number")
-st.image(wordcloud.to_array(), use_column_width=True)
+st.pyplot(plt)
