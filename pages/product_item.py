d8590b6efbf261d9416d8e4a59e4fdcdafe0583b
Rosie-Chenyr <146391133+Rosie-Chenyr@users.noreply.github.com>
2024-05-31 20:10:41 -0700

    Update product_item.py

--- pages/product_item.py
+++ pages/product_item.py
@@ -4,7 +4,7 @@ import plotly.express as px
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-@st.cache()
+@st.cache_data
 def load_data(path):
     data = pd.read_json('data/output_data_item.jsonl', lines=True, orient='records')
     data['average_rating'] = pd.to_numeric(data['average_rating'], errors='coerce')
@@ -16,23 +16,23 @@ def load_data(path):
 # read the data
 data = load_data('https://raw.githubusercontent.com/arashi520/Amazon-Review-Analysis/main/data/output_data_item.jsonl')
 
-data_cleaned = data.dropna(subset=['store'])
 
-# 筛选 rating_number 最多的前十个 store
+# filter the top ten stores with the most rating_number 
+data_cleaned = data[data['store'] != 'None']
 top_stores = data_cleaned['store'].value_counts().head(10)
 
-# 创建条形图显示前十个 store 的评分数量
+# Create a bar chart showing the number of ratings for the top ten stores
 st.write("### Top 10 Stores by Rating Number")
 st.bar_chart(top_stores)
 
 
-# 筛选 rating_number 大于 50 的产品，并绘制 average_rating 的分布图
+# Filter products with rating_number greater than 50 and plot the distribution of average_rating
 filtered_data = data[data['rating_number'] > 50]
 fig = px.histogram(filtered_data, x='average_rating', title='Distribution of Average Rating for Products with Rating Number > 50')
 st.write("### Distribution of Average Rating for Products with Rating Number > 50")
 st.plotly_chart(fig)
 
-# 筛选 rating_number 最多的前 100 个产品的 title，并生成 word cloud
+# Filter the titles of the top 100 products with the most rating_number and generate word cloud
 top_titles = data.sort_values('rating_number', ascending=False).head(100)['title']
 wordcloud = WordCloud(width=800, height=400, background_color ='white').generate(' '.join(top_titles))
 plt.imshow(wordcloud, interpolation='bilinear')
